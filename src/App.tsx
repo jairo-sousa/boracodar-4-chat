1e39fe1500c57e835df237fd081f60941af8933c
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-01-30 17:28:02 -0300

    style: finish ChatHeader

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { SendForm } from "./components/SendForm";
 
 export function App() {
 	return (
-		<Box className="App">
+		<Box className="App" w="85.93%" h="89.84%">
 			<Flex
 				direction="column"
 				fontFamily="Roboto, sans-serif"
 				color="white"
 				w="100%"
-				h="4100%"
+				h="100%"
 			>
 				<ChatHeader />
 				<Sents />
--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,19 +1,33 @@
-import { Flex, Heading, Image, Text, Box } from "@chakra-ui/react";
+import { Flex, Heading, Image, Text, Box, Button } from "@chakra-ui/react";
 
 export function ChatHeader() {
 	return (
-		<Flex>
-			<Image src="" alt="chat image" />
-			<Flex>
+		<Flex w="100%" justify="space-between">
+			<Image
+				src="src/assets/chatImage.svg"
+				alt="chat image"
+				w="4.8rem"
+				h="4.8rem"
+			/>
+			<Flex w="88.63%" direction="column">
 				<Heading as="h1" size="1.6rem">
 					Cecilia Sassaki
 				</Heading>
-				<Box>
-					<Box>.</Box>
-					<Text>Oline</Text>
-				</Box>
+				<Flex align="center" gap="0.4rem">
+					<Box
+						backgroundColor="#00B37E"
+						w="0.8rem"
+						h="0.8rem"
+						borderRadius="50%"
+					></Box>
+					<Text size="1.2rem" color="#00B37E">
+						Online
+					</Text>
+				</Flex>
 			</Flex>
-			<Image src="src/assets/close.svg" alt="close buton" />
+			<Button backgroundColor="transparent">
+				<Image src="src/assets/close.svg" alt="close buton" />
+			</Button>
 		</Flex>
 	);
 }
